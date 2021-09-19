9b98a4b6deba3a92c1fea1ce0bb7afe801679400
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2021-09-19 18:58:33 -0300

    Aplicação finalizada

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -25,15 +25,26 @@ export function TaskList() {
       isComplete: false,
     };
 
-    setTasks((prevState) => [...tasks, newTask]);
+    setTasks((prevState) => [...tasks, newTask]); // [...prevState, newTask] -> Melhor prática
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checked = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    );
+    setTasks(checked);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
